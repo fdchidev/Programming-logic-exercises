bf219723046cb31fa60cb97632cb7914be1f192e
Fernando Chi <zenchi11dev@gmail.com>
2025-05-16 23:09:55 -0600

    Se comienza a realizar los ejercicios

---
+++ 01_operadores_y_estructuras_de_control.py
@@ -0,0 +1,42 @@
+'''
+EJERCICIO:
+- Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+(Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+- Utilizando las operaciones con operadores que tú quieras, crea ejemplos
+que representen todos los tipos de estructuras de control que existan
+en tu lenguaje:
+Condicionales, iterativas, excepciones...
+- Debes hacer print por consola del resultado de todos los ejemplos.
+
+DIFICULTAD EXTRA(opcional):
+Crea un programa que imprima por consola todos los números comprendidos
+entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+
+Seguro que al revisar detenidamente las posibilidades has descubierto algo nuevo.
+'''
+
+# ESTRUCTURAS DE CONTROL
+'''
+Permiten modificar el flujo de la ejecución de un conjunto de instrucciones. 
+
+3 tipos básicos de control de flujo:
+
+* Control secuencial 
+* Control de selección
+* Control de repetición
+
+'''
+
+# OPERADORES ARITMETICOS
+print(f'Suma 10 + 3 = {10 + 3}')
+print(f'Resta 10 - 3 = {10 - 3}')
+print(f'Multiplicación 10 * 3 = {10 * 3}')
+print(f'División 10 / 3 = {10 / 3}')
+print(f'Módulo 10 % 3 = {10 % 3}')
+print(f'Exponente 10 ** 3 = {10 ** 3}')
+print(f'División entera 10 // 3 = {10 // 3}')
+
+
+# OPERADORES DE COMPARACIÓN
+print(f' ')
